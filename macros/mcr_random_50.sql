8972615bf6d1570acc068d588082180c4e692830
Alexander Fokin <alexander.fokin@improvado.io>
2023-03-14 15:24:05 -0300

    Added description for models

--- macros/mcr_random_50.sql
+++
@@ -1,7 +0,0 @@
---scheme: https://www.figma.com/file/xrmg3yb2Czynz5MNQzwTou/Dummy-Data-From-raw-to-MCDM?node-id=104%3A390&t=0DkhJaz9cQ6cUyLe-4
-
-{%- macro mcr_random_50() -%}
-        
-CAST(round(rand() / 4294967295 * 50 + 0.5) AS String)
-
-{%- endmacro -%}
